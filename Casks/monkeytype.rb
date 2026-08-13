e795125086a00ba58200c5c65cfb386cb0a1a81a
Sanjay Baskaran <sanjaysmack@gmail.com>
2026-08-13 13:34:29 -0600

    chore: update Monkeytype cask to 0.1.1

--- Casks/monkeytype.rb
+++ Casks/monkeytype.rb
@@ -1,11 +1,11 @@
 cask "monkeytype" do
-  version "0.4.10"
-  sha256 "32377ffcec05e79bc1efbc9c1912fa08bdebb206d0312275bcbf1e93a1a9b025"
+  version "0.1.1"
+  sha256 "716f74c0aa7db8ace1e8abed48058778e44fd7be3a63d2fd897ec3e7086aaaa1"
 
-  url "https://github.com/sanjayb-28/monkeytype-desktop/releases/download/v#{version}/Monkeytype_#{version}_aarch64.dmg"
+  url "https://github.com/sanjayb-28/monkeytype/releases/download/desktop-v#{version}/Monkeytype-#{version}-arm64.dmg"
   name "Monkeytype"
   desc "Offline Monkeytype typing test for Apple Silicon"
-  homepage "https://github.com/sanjayb-28/monkeytype-desktop"
+  homepage "https://github.com/sanjayb-28/monkeytype"
 
   depends_on arch: :arm64
   depends_on macos: :tahoe
@@ -19,6 +19,7 @@ cask "monkeytype" do
   end
 
   zap trash: [
+    "~/Library/Application Support/@monkeytype/desktop",
     "~/Library/Application Support/com.monkeytype.desktop",
     "~/Library/Preferences/com.monkeytype.desktop.plist",
     "~/Library/WebKit/com.monkeytype.desktop",
